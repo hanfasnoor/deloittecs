b88bf00546544d1dffaa00cac90069011353c4b6
Hanfas Noor <hanfas.noor@macys.com>
2019-01-27 16:18:49 +0530

    Added some test cases

--- deloitte-activity-scheduler/src/test/java/com/deloitte/events/deloitteactivityscheduler/ActivityScheduleServiceImplTest.java
+++ deloitte-activity-scheduler/src/test/java/com/deloitte/events/deloitteactivityscheduler/ActivityScheduleServiceImplTest.java
@@ -6,9 +6,13 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.time.LocalTime;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import org.junit.After;
 import org.junit.Before;
@@ -18,6 +22,7 @@ import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.deloitte.events.deloitteactivityscheduler.constants.Constants;
 import com.deloitte.events.deloitteactivityscheduler.model.Activity;
 import com.deloitte.events.deloitteactivityscheduler.service.ActivityScheduleServiceImpl;
 import com.deloitte.events.deloitteactivityscheduler.util.CommonUtil;
@@ -84,14 +89,47 @@ public class ActivityScheduleServiceImplTest {
 
 	@Test
 	public void testScheduleActivitiesWithProperInput() throws Exception {
-		try
-		{
-			Map<String, List<Activity>> scheduleMap=activityScheduleService.scheduleActivities(properInputFilePath);
-			assertTrue(scheduleMap.size()>0);
-			assertTrue(scheduleMap.get("Team 1").size()>0);
+		try {
+			Map<String, List<Activity>> scheduleMap = activityScheduleService.scheduleActivities(properInputFilePath);
+			assertTrue(scheduleMap.size() > 0);
+			assertTrue(scheduleMap.get("Team 1").size() > 0);
+		} catch (Exception e) {
+			assertFalse(true);
 		}
-		catch(Exception e)
-		{
+	}
+
+	@Test
+	public void testLastEventIsPresentationForMultipleTeamSchedule() throws Exception {
+		try {
+			Map<String, List<Activity>> scheduleMap = activityScheduleService.scheduleActivities(properInputFilePath);
+			assertTrue(scheduleMap.size() > 0);
+			assertTrue(scheduleMap.get("Team 1").size() > 0);
+			for (Entry<String, List<Activity>> schedule : scheduleMap.entrySet()) {
+				List<Activity> activityList = schedule.getValue();
+				Activity activity = activityList.stream().max(Comparator.comparing(Activity::getStartTime)).get();
+				assertTrue(activity.getStartTime().equals(Constants.END_MAX_TIME));
+				assertTrue(activity.getName().equals(Constants.STAFF_PRESENTATION));
+			}
+		} catch (Exception e) {
+			assertFalse(true);
+		}
+	}
+
+	@Test
+	public void testLucnhBreakInSchedule() throws Exception {
+		try {
+			Map<String, List<Activity>> scheduleMap = activityScheduleService.scheduleActivities(properInputFilePath);
+			assertTrue(scheduleMap.size() > 0);
+			assertTrue(scheduleMap.get("Team 1").size() > 0);
+			for (Entry<String, List<Activity>> schedule : scheduleMap.entrySet()) {
+				List<Activity> activityList = schedule.getValue();
+				List<Activity> activities = activityList.stream()
+						.filter(act -> act.getName().equals(Constants.LUNCH_BREAK)).collect(Collectors.toList());
+				assertTrue(activities != null && activities.size() == 1
+						&& activities.get(0).getName().equals(Constants.LUNCH_BREAK)
+						&& activities.get(0).getStartTime().isBefore(Constants.LUNCH_BREAK_TIME));
+			}
+		} catch (Exception e) {
 			assertFalse(true);
 		}
 	}
