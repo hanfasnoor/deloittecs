a320d4a84e0357408e3ba34309724c7b3ac5c021
Hanfas Noor <hanfas.noor@macys.com>
2019-01-28 08:20:41 +0530

    Added some comments to methods

--- deloitte-activity-scheduler/src/main/java/com/deloitte/events/deloitteactivityscheduler/service/ActivityScheduleServiceImpl.java
+++ deloitte-activity-scheduler/src/main/java/com/deloitte/events/deloitteactivityscheduler/service/ActivityScheduleServiceImpl.java
@@ -25,27 +25,28 @@ import com.deloitte.events.deloitteactivityscheduler.util.CommonUtil;
 
 @Component
 public class ActivityScheduleServiceImpl implements ActivityScheduleService {
-	
+
 	Logger logger = LoggerFactory.getLogger(ActivityScheduleServiceImpl.class);
 
 	/**
 	 * Main public method for scheduling activities
 	 */
 	@Override
-	public Map<String,List<Activity>> scheduleActivities(String inputActivitiesTextPath) {
+	public Map<String, List<Activity>> scheduleActivities(String inputActivitiesTextPath) {
 		Stack<Activity> activityStack = CommonUtil.processInputActivitiesFile(inputActivitiesTextPath);
 		logger.info("No of activities " + (activityStack != null ? activityStack.size() : 0));
 		return scheduleActivitiesToTeams(activityStack);
-		
+
 	}
 
-	
 	/**
-	 * This method will run through the stack of Activities and dynamically creates Teams and assign activity List
+	 * This method will run through the stack of Activities and dynamically creates
+	 * Teams and assign activity List
+	 * 
 	 * @param activityStack
 	 * @return Map of Teams vs ActivityList
 	 */
-	private Map<String,List<Activity>> scheduleActivitiesToTeams(Stack<Activity> activityStack) {
+	private Map<String, List<Activity>> scheduleActivitiesToTeams(Stack<Activity> activityStack) {
 
 		if (null == activityStack || activityStack.isEmpty()) {
 			logger.info("No Activities to schedule");
@@ -65,15 +66,19 @@ public class ActivityScheduleServiceImpl implements ActivityScheduleService {
 			boolean isLunchOver = false;
 
 			String teamName = "Team " + teamCounter;
-			while (!maxEndTimeReached && !activityStack.isEmpty()) {
+			while (!maxEndTimeReached && !activityStack.isEmpty()) { // This while loop exit when max end time (5:00 PM)
+																		// is reached or activities are over.It will
+																		// create a map of Team Vs ActivitySchedule
+																		// before exiting.
 				Activity activityItem = activityStack.pop();
 				long durationOfCurrentActivity = activityItem.getDuration();
 
 				LocalTime endTimeOfCurrentActivity = starTime.plusMinutes(durationOfCurrentActivity);
 				if (endTimeOfCurrentActivity.compareTo(Constants.END_MAX_TIME) <= 0) {
 
-					if (endTimeOfCurrentActivity.compareTo(Constants.LUNCH_BREAK_TIME) >= 0
-							&& !isLunchOver) {
+					// This is to schedule Lunch break, it will happen only once per Team using
+					// boolean isLunchOver
+					if (endTimeOfCurrentActivity.compareTo(Constants.LUNCH_BREAK_TIME) >= 0 && !isLunchOver) {
 						activityStack.push(activityItem);
 						isLunchOver = true;
 						durationOfCurrentActivity = Constants.LUNCH_BREAK_DURATION;
@@ -87,19 +92,24 @@ public class ActivityScheduleServiceImpl implements ActivityScheduleService {
 						starTime = endTimeOfCurrentActivity;
 					}
 
+					// Activities can be finished before evening 5:00 PM, this is to handle that
+					// case
 					if (activityStack.isEmpty()) {
-						activitiesForTeam.add(new Activity(Constants.STAFF_PRESENTATION, Constants.STAFF_PRESENTATION_DURATION,
-								(Constants.END_MAX_TIME.equals(presentationStartTime) ? Constants.END_MAX_TIME
-										: (starTime.isAfter(Constants.EARLY_PRESENTATION_START_TIME) ? starTime
-												: Constants.EARLY_PRESENTATION_START_TIME))));
+						activitiesForTeam
+								.add(new Activity(Constants.STAFF_PRESENTATION, Constants.STAFF_PRESENTATION_DURATION,
+										(Constants.END_MAX_TIME.equals(presentationStartTime) ? Constants.END_MAX_TIME
+												: (starTime.isAfter(Constants.EARLY_PRESENTATION_START_TIME) ? starTime
+														: Constants.EARLY_PRESENTATION_START_TIME))));
 						teamActivityMap.put(teamName, activitiesForTeam);
 						break;
 					}
 
-				} else {
+				} else { // Once max end time 5:00 PM is reached exiting the loop and schedule Staff
+							// Presentation and putting all activities to map for a Team
 					maxEndTimeReached = true;
 					presentationStartTime = Constants.END_MAX_TIME;
-					activitiesForTeam.add(new Activity(Constants.STAFF_PRESENTATION, Constants.STAFF_PRESENTATION_DURATION, Constants.END_MAX_TIME));
+					activitiesForTeam.add(new Activity(Constants.STAFF_PRESENTATION,
+							Constants.STAFF_PRESENTATION_DURATION, Constants.END_MAX_TIME));
 					teamActivityMap.put(teamName, activitiesForTeam);
 					activityStack.push(activityItem);
 					teamCounter++;
@@ -110,14 +120,14 @@ public class ActivityScheduleServiceImpl implements ActivityScheduleService {
 		}
 
 		printScheduleToConsole(teamActivityMap);
-		
+
 		return teamActivityMap;
 
 	}
 
-
 	/**
 	 * Helper method to iterate over final map and print schedule in pattern
+	 * 
 	 * @param teamActivityMap
 	 */
 	private void printScheduleToConsole(Map<String, List<Activity>> teamActivityMap) {
@@ -125,13 +135,11 @@ public class ActivityScheduleServiceImpl implements ActivityScheduleService {
 			logger.info(schedule.getKey());
 			logger.info("-------------------------");
 			for (Activity activity : schedule.getValue()) {
-				logger.info(
-						activity.getStartTime() + " : " + activity.getName() + " " + activity.getDuration() + Constants.MINUTES);
+				logger.info(activity.getStartTime() + " : " + activity.getName() + " " + activity.getDuration()
+						+ Constants.MINUTES);
 			}
 			System.out.println();
 		}
 	}
-
-	
 
 }
